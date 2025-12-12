96b8c3bb6e25563bca6fc59499f21e25da84311f
aakarshsingh <singhaakarsh1506@gmail.com>
2025-12-13 01:55:46 +0530

    Arrays in JS

---
+++ arr.js
@@ -0,0 +1,13 @@
+//Arrays in Javascript
+let selectedColors = ['Red', 'Blue', 'Black', 'Sky'];//Array Initialisation
+console.log(selectedColors);
+
+console.log(selectedColors[0]);//Printing Array Element 1
+console.log(selectedColors[2]);//Printing Array Element 3
+
+
+selectedColors[4] = 'Green';//Length of the array can be increased directly
+console.log(selectedColors);
+
+//When we make an array in JS it automatically gets some properties like lenght etc
+console.log(selectedColors.length);
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+//Basics in JavaScript
+
 console.log("Hello, World!");
 let name = "Aakarsh Singh";
 console.log(name);
 console.log("Hi, How are you?");
+
+// We use 'let' for a variable if we need to change the value of the variable else we have ti use 'const'
+
+let myName = 'Aakarsh Singh' //String Literal
+let age = 18; // Number Literal
+let isApproved = true; // Boolean Literal (true/false)
+let firstName = undefined; // Undefined Literal or we can just write let firstName;
+let color = null; //Null Literal
