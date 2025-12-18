790608e8095c75ce51c077a315f66839c7abfae8
Aakarsh Singh <singhaakarsh1506@gmail.com>
2025-12-19 01:09:53 +0530

    ES6 Modules in JS

--- mod-js.js
+++ mod-js.js
@@ -1,1 +1,15 @@
-//ES6 Modules in JS
+//ES6 Modules in JavaScript
+
+
+import {PI, getCircumference, getArea, getVolume} from './mathUtil.js'; //Importing the pre written code from the file mathUtil.js
+
+console.log(PI);
+
+let val1 = getCircumference(10);
+console.log(`${val1.toFixed(2)}cm`);//Using .toFixed function to fix the value to 2 decimal places
+
+let val2 = getArea(10);
+console.log(`${val2.toFixed(2)}cm`);
+
+let val3 = getVolume(10);
+console.log(`${val3.toFixed(2)}cm`);
