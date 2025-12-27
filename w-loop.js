2c48ffdd62d378027802b25848332ed50203b421
Aakarsh Singh <singhaakarsh1506@gmail.com>
2025-12-28 00:31:17 +0530

    While & Do-While loops in JavaScript

---
+++ w-loop.js
@@ -0,0 +1,50 @@
+//While Loops in JavaScript
+
+//While loops work while the given condition is true, as soon as the condition changes the loop breaks
+
+let i = 1;
+while(i<=5){
+    console.log("Hello, World!");
+    i++;
+}
+
+let username = "";
+
+while(username===""){
+    username = window.prompt('Enter Your Name: '); // window.prompt is an inpit command for the user to input values whil the program works
+}
+
+console.log(`Hello ${username}`); 
+
+//Do-While loops in Javacript
+
+// In do while loops the condition gets executed atleast once and then checks the while condition
+
+
+let age;
+
+do{
+    age = window.prompt('Enter your Age: '); //The function is executed atleast once before the condition is checked
+}while(age==="" || age===null)
+    console.log(`Your age is ${age} yrs`);
+
+
+// Basic login using window.prompt code
+
+let loggedIn = false;
+let user;
+let password;
+
+while(!loggedIn){
+
+    user = window.prompt('Enter your Username: ');
+    password = window.prompt('Enter you Password: ');
+
+    if(user === "Aakarsh1506" && password === "pass@123"){
+        loggedIn = true;
+        console.log("Your are logged In, Welcome!");
+    }
+    else{
+        console.log("Invalid Credentials, Please try again!!");
+    }
+}
